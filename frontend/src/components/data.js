e45187646c1a6843eae6fb8d997d0a1c6a33fb29
comconToukir <ahmdtoukir@gmail.com>
2022-04-19 12:33:30 +0600

    first commit

---
+++ frontend/src/components/data.js
@@ -0,0 +1,38 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dzrf760el/image/upload/v1650226954/Carousel/yyyyy_1_afuzmj.webp",
+    title: "LAPTOP SALE",
+    desc: "NEW LAPTOPS 19% OFF FOR LIMITED TIME!",
+    bg: "FFFFFF",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dzrf760el/image/upload/v1650226953/Carousel/1647238315448_kyadts.png",
+    title: "MOBILE COLLECTION",
+    desc: "NEW SMARTPHONES 19% OFF FOR LIMITED TIME! DON'T MISS THIS.",
+    bg: "FFFFFF",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dzrf760el/image/upload/v1650226947/Carousel/6479739_rd_yyf38p.jpg",
+    title: "DESKTOP OFFERS",
+    desc: "STYLE AND PERFORMANCE GO TOGETHER. 25% OFF!",
+    bg: "FFFFFF",
+  },
+];
+
+export const categories = [
+  'Electronics',
+  'Cameras',
+  'Laptops',
+  'Accessories',
+  'Headphones',
+  'Food',
+  "Books",
+  'Clothes/Shoes',
+  'Beauty/Health',
+  'Sports',
+  'Outdoor',
+  'Home'
+]
