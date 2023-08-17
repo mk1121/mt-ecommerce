e4a56147ed8cca9b5a5a00afc43619a124da1d5e
mk1121 <marufm.3666@gmail.com>
2023-08-17 08:58:41 +0600

    fix: products images url
    -add a script for validate  images url in product section
    -add a favicon

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,20 +1,37 @@
+// to run the script use node vesion:18
+const fs = require("fs");
 const Product = require("../models/product");
 const dotenv = require("dotenv");
 const connectDatabase = require("../config/database");
-
-// const products = require("../data/products.json");
-const products = require("../data/chaldalProducts.json");
-// Setting dotenv file
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
 const seedProducts = async () => {
   try {
+    // product delete from db
     await Product.deleteMany();
     console.log("Products are deleted");
 
-    await Product.insertMany(products);
+    //check  images url response valid or not
+    const data = JSON.parse(fs.readFileSync("backend/data/chaldalProducts.json"));
+    let tempData = [];
+
+    console.log('images response validate')
+    for (let i = 0; i < data.length; i++) {
+      const url = data[i].images[0].url;
+      const response = await fetch(url);
+
+      if (response.status === 200) {
+        // Iterate over the existing data.
+        console.log(i, 'of', data.length, 'image response status 200');
+        tempData.push(data[i])
+      }
+    }
+    const jsonString = JSON.stringify(tempData, null, 2);
+
+    // product update in db
+    await Product.insertMany(tempData);
     console.log("All Products are added.");
 
     process.exit();
